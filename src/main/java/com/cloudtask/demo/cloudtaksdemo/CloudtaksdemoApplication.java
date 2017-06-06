6f6420a4ee972977facd613bbc590247becfaf56
Ahmad Jawid Jami <ahmadjawid@Ahmads-MacBook-Pro.local>
2017-06-06 19:39:10 +0200

    t

--- src/main/java/com/cloudtask/demo/cloudtaksdemo/CloudtaksdemoApplication.java
+++ src/main/java/com/cloudtask/demo/cloudtaksdemo/CloudtaksdemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.Bean;
 import org.springframework.util.StringUtils;
@@ -16,6 +17,7 @@ import java.util.Date;
 
 @SpringBootApplication
 @EnableTask
+@EnableConfigurationProperties
 public class CloudtaksdemoApplication {
 
 	@Bean
