a7660914e2ed8eab74554f438a69610a43285452
Ahmad Jawid Jami <ahmadjawid@Ahmads-MacBook-Pro.local>
2017-06-06 20:23:15 +0200

    the

---
+++ src/main/java/org/springframework/cloud/task/app/timestamp/Application.java
@@ -0,0 +1,17 @@
+package org.springframework.cloud.task.app.timestamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by ahmadjawid on 6/6/17.
+ */
+//@SpringBootApplication
+public class Application {
+
+//    public static void main(String[] args) {
+//
+//        SpringApplication.run(Application.class, args);
+//
+//    }
+}
